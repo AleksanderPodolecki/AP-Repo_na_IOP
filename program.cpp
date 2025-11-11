a5c5ef08dae809ea1d34e7cd7d6a0fc3338c49cf
Olek <opodolecki@gmail.com>
2025-11-11 15:09:23 +0100

    Dodano sprawdzanie nieujemnosci liczby

--- program.cpp
+++ program.cpp
@@ -4,7 +4,14 @@ using namespace std;
 int main(){
 	int x;
 	cin >> x;
-	cout << endl << x << endl;
+	cout << endl;
+
+	if(x<0){
+		cout << "Blad: wczytana liczba jest ujemna!!!!" << endl;
+		return 0;
+	}
+
+	cout << "Wczytana liczba: " << x << endl;
 	
 	return 0;
 }
